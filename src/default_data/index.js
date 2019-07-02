5b139cf038a5abb5e83278e68cb3a7c3964a5291
Kramerbaf <nds.kram@gmail.com>
2019-07-02 14:29:08 +0200

    cloned from card-slider

---
+++ src/default_data/index.js
@@ -0,0 +1,13 @@
+module.exports = [
+    {
+        name: 'card_slider',
+        category: 'features',
+        storeName: {
+            en: "Slider card section",
+            fr: "Slider card section",
+        },
+        storeDesc: {
+            en_GB: "Slider section with swipe features"
+        }
+    }
+] 
